3c8c8f91bb6617e62ebb0af6ed0cab136c8e9da1
StickManJ0e <121269062+StickManJ0e@users.noreply.github.com>
2023-04-17 09:30:23 +1000

    Initialise React App and config

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './css/App.css';
+
+const App = () => {
+  return (
+    <div>Hello World!</div>
+  )
+}
+
+export default App;
---
+++ src/setupTests.js
@@ -0,0 +1,4 @@
+import React from "react";
+import * as name from "@testing-library/react";
+import "@testing-library/jest-dom";  // optional
+import userEvent from "@testing-library/user-event";
