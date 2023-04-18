50f9f3de2007df1ce851862e2e607dec70bbc56e
StickManJ0e <121269062+StickManJ0e@users.noreply.github.com>
2023-04-19 09:13:24 +1000

    Init and host

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBbA1wN2AGli-tZlv_SBO-0kdZtC3OjhFM",
+  authDomain: "where-is-waldo-64ca4.firebaseapp.com",
+  projectId: "where-is-waldo-64ca4",
+  storageBucket: "where-is-waldo-64ca4.appspot.com",
+  messagingSenderId: "968276203608",
+  appId: "1:968276203608:web:0580093fd982b38fe4122d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
